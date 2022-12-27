7683ff460fbae514c98d7ebc28b9ad54070a5a04
Filip Pitura <filippitura123@gmail.com>
2022-12-27 14:31:47 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+{
+    const formElement = document.querySelector(".js-form");
+    const currencyElement = document.querySelector(".js-currency");
+    const valueElement = document.querySelector(".js-value");
+    const resultElement = document.querySelector(".js-result");
+
+    const RateUSD = 0.229
+    const RateEUR = 0.215
+    const RateJPY = 30.433
+
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const value = +valueElement.value;
+        const currency = currencyElement.value;
+
+        let result;
+
+        switch (currency) {
+            case "USD":
+                result = value * RateUSD;
+                break;
+
+            case "EUR":
+                result = value * RateEUR;
+                break;
+
+            case "JPY":
+                result = value * RateJPY;
+        }
+
+        resultElement.innerText = `${result.toFixed(2)}`
+
+    });
+}
